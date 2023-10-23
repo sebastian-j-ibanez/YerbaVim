aa5c65eaf86db555229e0d4928213d63071f384f
sebastian-j-ibanez <sebas.ibanez219@gmail.com>
2023-10-23 12:38:00 -0400

    Added colorschemes, added lush

--- lua/zzz/plugins/colorscheme.lua
+++ lua/zzz/plugins/colorscheme.lua
@@ -2,11 +2,12 @@ return {
     {
         "EdenEast/nightfox.nvim",
     },
-
     {
         "Shatur/neovim-ayu",
     },
-
+    {
+        "mcchrish/zenbones.nvim",
+    },
     {
         "nyoom-engineering/oxocarbon.nvim",
         priority = 1000,
