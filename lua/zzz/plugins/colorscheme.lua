0bb6478aa92ada43e9b3e0cbd5507d23f0d87d70
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2024-11-23 21:50:38 -0500

    fix: remove ununsed colorschemes

--- lua/zzz/plugins/colorscheme.lua
+++ lua/zzz/plugins/colorscheme.lua
@@ -1,28 +1,4 @@
 return {
-    -- Miasma
-    {
-        "xero/miasma.nvim",
-        branch = "dev",
-        dependencies = {
-            "rktjmp/lush.nvim",
-            "rktjmp/shipwright.nvim",
-        },
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.cmd("colorscheme miasma")
-        end,
-    },
-    --Nord
-    {
-        "shaunsingh/nord.nvim",
-        config = function()
-            vim.g.nord_borders = true
-            vim.g.nord_italic = false
-            vim.g.nord_cursorline_transparent = true
-            require('nord').set()
-        end,
-    },
     -- Poimandres
     {
         'olivercederborg/poimandres.nvim',
@@ -33,14 +9,13 @@ return {
     -- Rasmus
     {
         'kvrohit/rasmus.nvim',
+        config = function()
+            vim.g.rasmus_bold_functions = true
+        end,
     },
     -- Skull
     {
         'kxzk/skull-vim',
-    },
-    -- VS Code
-    {
-        "askfiy/visual_studio_code",
     },
 }
 
