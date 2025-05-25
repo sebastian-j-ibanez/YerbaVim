7ebca1b8b47ce29cd8e6b6bfa86f7bd676a62a16
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-05-24 21:23:15 -0400

    feat: add node_modules and .venv to telescope ignore

--- lua/zzz/plugins/telescope.lua
+++ lua/zzz/plugins/telescope.lua
@@ -1,18 +1,20 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-      defaults = {
-        layout_strategy = 'horizontal',
-        sorting_strategy = 'ascending',
-        file_ignore_patterns = {
-            ".git",
-            "target",
-            ".idea",
-            ".vscode"
-        }
-      },
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        opts = {
+            defaults = {
+                layout_strategy = 'horizontal',
+                sorting_strategy = 'ascending',
+                file_ignore_patterns = {
+                    ".git",
+                    "target",
+                    ".idea",
+                    ".vscode",
+                    "node_modules",
+                    ".venv"
+                }
+            },
+        },
     },
-  },
 }
