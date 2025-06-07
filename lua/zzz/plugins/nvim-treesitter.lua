158826091fcdc96dd175a410482d3ae2269644d0
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-06-07 19:42:11 -0400

    feat: enable treesitter sync install

--- lua/zzz/plugins/nvim-treesitter.lua
+++ lua/zzz/plugins/nvim-treesitter.lua
@@ -5,8 +5,8 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "cpp", "go", "java", "python", "javascript", "html", "lua" },
-          sync_install = false,
+          ensure_installed = { "c", "cpp", "go", "rust", "python", "javascript", "html", "lua" },
+          sync_install = true,
           highlight = { enable = true },
           indent = { enable = true },
         })
