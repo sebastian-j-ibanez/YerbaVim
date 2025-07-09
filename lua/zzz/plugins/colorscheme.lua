72cf328de126b01c2ee0455a9e9aef0f9799497f
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-07-09 12:58:34 -0400

    theme: remove all italics in modus theme

--- lua/zzz/plugins/colorscheme.lua
+++ lua/zzz/plugins/colorscheme.lua
@@ -5,7 +5,17 @@ return {
     {
         "miikanissi/modus-themes.nvim",
         lazy = false,
-        priority = 1000
+        priority = 1000,
+        config = function ()
+            require("modus-themes").setup({
+                styles = {
+                    comments = { italic = false},
+                    keywords = { italic = false},
+                    functions = { italic = false },
+                    variables = { italic = false },
+                },
+            })
+        end
     }
 }
 
