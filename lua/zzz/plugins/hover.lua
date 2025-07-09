8e1358d48e26e1a804cfa865a903da17eade4ad8
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-07-09 12:59:30 -0400

    feat: add dap server support

--- lua/zzz/plugins/hover.lua
+++ lua/zzz/plugins/hover.lua
@@ -5,14 +5,7 @@ return {
             init = function()
                 -- Require providers
                 require("hover.providers.lsp")
-                -- require('hover.providers.gh')
-                -- require('hover.providers.gh_user')
-                -- require('hover.providers.jira')
-                -- require('hover.providers.dap')
-                -- require('hover.providers.fold_preview')
-                -- require('hover.providers.diagnostic')
-                -- require('hover.providers.man')
-                -- require('hover.providers.dictionary')
+                require('hover.providers.dap')
             end,
             preview_opts = {
                 border = "single"
