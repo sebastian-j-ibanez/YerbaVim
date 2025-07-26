bdb250a536e52259de4b8a7922cb0f7efd439ec2
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-07-26 19:22:31 -0400

    feat: add ocaml + haskell treesitter, add fleet theme

--- lua/zzz/plugins/nvim-treesitter.lua
+++ lua/zzz/plugins/nvim-treesitter.lua
@@ -2,13 +2,13 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ":TSUpdate",
     config = function ()
-      local configs = require("nvim-treesitter.configs")
+        local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "c", "cpp", "go", "rust", "python", "javascript", "html", "lua" },
-          sync_install = true,
-          highlight = { enable = true },
-          indent = { enable = true },
+        configs.setup({
+            ensure_installed = { "lua", "c", "cpp", "go", "rust", "python", "javascript", "html", "ocaml", "haskell" },
+            sync_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
         })
     end
 }
